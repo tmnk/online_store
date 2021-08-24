8a3b59bc91afcdcfe1c720b522004fe818529fcc
tmnk <temnikovgiga@yandex.ru>
2021-08-24 12:38:03 +0300

    fix: connect to pg

---
+++ build/app/db.js
@@ -0,0 +1,10 @@
+"use strict";
+var Sequelize = require('sequelize').Sequelize;
+module.exports = new Sequelize(process.env.DB_NAME, // Название БД
+process.env.DB_USER, // Пользователь
+process.env.DB_PASSWORD, // ПАРОЛЬ
+{
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
+});
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,31 @@
 import * as express from 'express'
 import { config } from 'dotenv'
 import { Request, Response, Application } from 'express'
-
 config()
+const sequelize = require('./db')
+
 
 const PORT = process.env.PORT || 8080
 
 const app: Application = express()
 
-app.listen(PORT, () => console.log(`Server work on ${PORT}`))
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("hello")
+})
+
+const startApp = async () => {
+    try {
+        console.log(1)
+        await sequelize.authenticate()
+        
+        console.log(2)
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server work on ${PORT}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startApp()
